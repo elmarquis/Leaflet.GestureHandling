e7745b1d28c5659b0443cf3301a81e3c0da0d33d
Dzulqarnain Nasir <dzul1983@gmail.com>
2019-05-29 10:37:18 +0300

    Updated dist files

--- dist/leaflet-gesture-handling.js
+++ dist/leaflet-gesture-handling.js
@@ -330,12 +330,9 @@
     * * Version 1.1.8
     */
 
-    L.Map.mergeOptions({
-        gestureHandlingOptions: {
-            text: {},
-            duration: 1000
-        }
-    });
+    var gestureHandlingOptions = {
+        duration: 1000
+    };
 
     var draggingMap = false;
 
@@ -410,8 +407,12 @@
         _setupPluginOptions: function () {
             //For backwards compatibility, merge gestureHandlingText into the new options object
             if (this._map.options.gestureHandlingText) {
-                this._map.options.gestureHandlingOptions.text = this._map.options.gestureHandlingText;
+                this._map.options.gestureHandlingOptions = L.extend(gestureHandlingOptions, {
+                    text: this._map.options.gestureHandlingText
+                });
             }
+
+            this._map.options.gestureHandlingOptions = L.extend(gestureHandlingOptions, this._map.options.gestureHandlingOptions);
         },
 
         _setLanguageContent: function () {
